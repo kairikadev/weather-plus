6adf6a7a66826696b3aec601e8f693f5f4b63602
kairikadev <80520850+kairikadev@users.noreply.github.com>
2021-05-18 12:58:03 +0200

    get the date function in process

--- vanilla-weather/src/script.js
+++ vanilla-weather/src/script.js
@@ -1,15 +1,24 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes =date.getMinutes();
+    let day = day.getDay();
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML =Math.round(response.data.main.temp);
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML =response.data.name;
     let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML = response.data.weather[0].description;
     let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date")
     windElement.innerHTML =Math.round(response.data.wind.speed);
-    
+    temperatureElement.innerHTML =Math.round(response.data.main.temp);
+    cityElement.innerHTML =response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    dateElement.innerHTML = formatDate(response.data.dt*1000);
 }
 
 let apiKey = "e1011e97bf969d1b569c2b62944075b5 ";
