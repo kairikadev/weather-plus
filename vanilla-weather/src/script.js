f5d9afb3b1ad627a259967423d0635c697bfa9f4
kairikadev <80520850+kairikadev@users.noreply.github.com>
2021-05-18 16:12:31 +0200

    Added relevant icon from API

--- vanilla-weather/src/script.js
+++ vanilla-weather/src/script.js
@@ -1,9 +1,19 @@
 function formatDate(timestamp){
+    //calculate the date
     let date = new Date(timestamp);
     let hours = date.getHours();
     let minutes =date.getMinutes();
-    let day = day.getDay();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"];
+    let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
+   
 }
 
 function displayTemperature(response){
@@ -12,13 +22,17 @@ function displayTemperature(response){
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
-    let dateElement = document.querySelector("#date")
+    let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+
     windElement.innerHTML =Math.round(response.data.wind.speed);
     temperatureElement.innerHTML =Math.round(response.data.main.temp);
     cityElement.innerHTML =response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     dateElement.innerHTML = formatDate(response.data.dt*1000);
+    iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt",response.data.weather[0].description);
 }
 
 let apiKey = "e1011e97bf969d1b569c2b62944075b5 ";
