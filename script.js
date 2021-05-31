525dae48755c9f9a5b0a22ed24f2141e847d0c78
kairikadev <80520850+kairikadev@users.noreply.github.com>
2021-05-31 12:17:28 +0200

    Adding the forecast and icons in JS

--- script.js
+++ script.js
@@ -16,21 +16,67 @@
      .get(apiUrl)
      .then(processResponse);
  }
+
+ function formatDay(timestamp){
+  let date = new Date(timestamp *1000);
+  let day = date.getDay();
+  let days =["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+  return days[day];
+}
+function displayForecast(response){
+     
+  let forecastElement = document.querySelector(".forecast");
+  
+  let forecast = response.data.daily;
+  let forecastHTML = `<div class="row">`;
+ 
+  forecast.forEach(function(forecastDay, index){
+      if(index < 5){
+      forecastHTML = forecastHTML +
+      `<div class="weekdays" id="weekdays">
+        <div class="col-2">  
+            <div class="forecast-day">${formatDay(forecastDay.dt)}</div> 
+              <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+              width="42"/>
+             <div class="forecast-temperatures">
+                 <span class="forecast-degrees-min">${Math.round(forecastDay.temp.max)}</span>
+                 <span class="forecast-degrees-max">${Math.round(forecastDay.temp.min)}</span>
+             </div> 
+             </div>
+             </div>
+          
+      
+      `; }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+ function getForecast(coordinates){
+  let apiKey = "e1011e97bf969d1b569c2b62944075b5 ";
+  let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=e1011e97bf969d1b569c2b62944075b5&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
  
  function processResponse(response){
    let temperature = Math.round(response.data.main.temp);
    let degrees=document.querySelector("#degrees-number");
-   degrees.innerHTML = `${temperature} &#176;`;
    let humidityElement = document.querySelector("#humidity");
-    let windElement = document.querySelector("#wind");
-    windElement.innerHTML =Math.round(response.data.wind.speed);
-    humidityElement.innerHTML = response.data.main.humidity;
+   let windElement = document.querySelector("#wind");
    let weatherDescription = response.data.weather[0].description;
    let description= document.querySelector("#description");
-   description.innerHTML = weatherDescription;
    let cityName = response.data.name;
    let title = document.querySelector("h1");
+  
+   degrees.innerHTML = `${temperature} &#176;`;
+   windElement.innerHTML =Math.round(response.data.wind.speed);
+   humidityElement.innerHTML = response.data.main.humidity;
+   description.innerHTML = weatherDescription;
    title.innerHTML = cityName;
+
+   getForecast(response.data.coord);
  }
  
  let searchForm = document.querySelector("#search-city");
